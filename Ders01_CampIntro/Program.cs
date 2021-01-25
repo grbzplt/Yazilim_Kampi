7960053a2d80152eed40b698a56f7cd8bc1231e0
Grbz Plt <grbzplt@hotmail.com>
2021-01-25 14:16:41 +0300

    Ders01 Eklendi

---
+++ Ders01_CampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ders01_CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+
+            Console.WriteLine(kategoriEtiketi);
+
+            int ogrenciSayisi = 32000;
+
+            double faizOrani = 1.45;
+
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+        }
+    }
+}
